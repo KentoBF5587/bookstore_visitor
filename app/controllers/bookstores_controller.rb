7d6773dd5375b52fa2476ed3be663abdeb698bf8
福田健人 <fukudakento@fukudataketonoMacBook-Air.local>
2024-05-18 14:59:39 +0900

    書店投稿機能・編集・削除機能を実装しました

--- app/controllers/bookstores_controller.rb
+++ app/controllers/bookstores_controller.rb
@@ -1,5 +1,6 @@
 class BookstoresController < ApplicationController
   skip_before_action :require_login, only: %i[index show]
+  before_action :set_bookstore, only: %i[edit update destroy]
   
   def index
     @bookstores = Bookstore.includes(:user).order(created_at: :desc)
@@ -19,13 +20,15 @@ class BookstoresController < ApplicationController
     end
   end
 
-  def show; end
+  def show
+    @bookstore = Bookstore.find(params[:id])
+  end
 
   def edit; end
 
   def update
     if @bookstore.update(bookstore_params)
-      redirect_to bookstore_url(@bookstore), success: t('.success')
+      redirect_to bookstore_path(@bookstore), success: t('.success')
     else
       flash.now[:danger] = t('.failure')
       render :edit, status: :unprocessable_entity
@@ -33,13 +36,17 @@ class BookstoresController < ApplicationController
   end
 
   def destroy
-    @board.destroy!
-    redirect_to bookstores_path, success: "あああああ", status: :see_other
+    @bookstore.destroy!
+    redirect_to bookstores_path, success: t('.success'), status: :see_other
   end
 
   private
 
   def bookstore_params
     params.require(:bookstore).permit(:name, :address)
+  end
+
+  def set_bookstore
+    @bookstore = Bookstore.find(params[:id])
   end
 end

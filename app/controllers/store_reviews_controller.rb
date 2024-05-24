f7247d5ae084eec591045c782a8b9dba7746f903
福田健人 <fukudakento@fukudataketonoMacBook-Air.local>
2024-05-24 15:09:38 +0900

    投稿ページの編集と削除機能をつけました

--- app/controllers/store_reviews_controller.rb
+++ app/controllers/store_reviews_controller.rb
@@ -1,7 +1,7 @@
 class StoreReviewsController < ApplicationController
   skip_before_action :require_login, only: %i[show]
   before_action :set_store_review, only: %i[edit update destroy]
-  
+
   def show
     @store_review = StoreReview.find(params[:id])
   end
@@ -18,31 +18,34 @@ class StoreReviewsController < ApplicationController
     @store_review = @bookstore.store_reviews.build(store_review_params)
     @store_review.user = current_user
 
-  if @store_review.save
-    redirect_to bookstore_url(@bookstore), success: "あああああ"
-  else
-    render :new, status: :unprocessable_entity
-  end
+    if @store_review.save
+      redirect_to bookstore_url(@bookstore), success: t('.success')
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
     if @store_review.update(store_review_params)
-      redirect_to bookstore_url(@bookstore)
+      redirect_to @store_review.bookstore, success: t('.success')
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
-  def destroy; end
+  def destroy
+    @store_review.destroy!
+    redirect_to bookstore_path(@bookstore), success: t('.success')
+  end
 
   private
 
   def set_store_review
-    @bookstore = Bookstore.find(params[:id])
     @store_review = current_user.store_reviews.find(params[:id])
+    @bookstore = Bookstore.find(@store_review.bookstore_id)
   end
 
   def store_review_params
-    params.require(:store_review).permit(:date_entered, :opportunity, :first_visit, :favorite_reason, :free_description).merge(bookstore_id: params[:bookstore_id])
+    params.require(:store_review).permit(:date_entered, :opportunity, :first_visit, :favorite_reason, :free_description)
   end
 end

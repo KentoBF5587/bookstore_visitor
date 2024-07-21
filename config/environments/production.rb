303393958ce1c8fcc5c45fcaa8450f5ca4711f78
福田健人 <fukudakento@fukudataketonoMacBook-Air.local>
2024-07-21 17:27:05 +0900

    パスワードリセットが本番環境でも動くように設定しました

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,11 +78,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
     domain:               'gmail.com',
-    user_name:            Rails.application.credentials.gmail[:user_name],
-    password:             Rails.application.credentials.gmail[:password],
-    authentication:       :login
+    port:                 587,
+    user_name:            Rails.application.credentials.dig(:gmail, :user_name),
+    password:             Rails.application.credentials.dig(:gmail, :password),
+    authentication:      :login,
+    enable_starttls_auto: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
